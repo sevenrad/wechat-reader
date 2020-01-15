33b3a9a26d395c70d069b79aaff6d47ec4871fe9
zhoubingcan <zhoubingcan@360.net>
2020-01-15 17:42:05 +0800

    完成项目初始化配置

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <span class="text">ABCDETF</span>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+  export default {
+    computed: {
+      ...mapGetters(['test'])
+    },
+    mounted () {
+      this.$store.dispatch('setTest', 10).then(() => {
+        console.log(this.test)
+      })
+    }
+  }
+  document.addEventListener('DOMContentLoaded', () => {
+    const html = document.querySelector('html')
+    let fontSize = window.innerWidth / 10
+    fontSize = fontSize > 50 ? 50 : fontSize
+    html.style.fontSize = fontSize + 'px'
+  })
+</script>
+
+<style lang="scss" scoped>
+  @import "./assets/styles/global.scss";
+  .text{
+    font-family: 'Days One';
+    font-size: px2rem(20);
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <span class="icon-bookmark"></span>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Epub from 'epubjs'
+global.epub = Epub
+export default {
+}
+</script>
+
+<style scoped>
+  .icon-bookmark {
+    font-size: 40px;
+  }
+</style>
